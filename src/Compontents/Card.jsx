4b0014af9c06b0c76bc881b1ffb1e739910b2ddc
Tuhin Pramanik <156042548+TuhinPramanik4@users.noreply.github.com>
2025-01-18 10:29:43 +0530

    add drag fetures to the cards

--- src/Compontents/Card.jsx
+++ src/Compontents/Card.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import { FaFileAlt } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
-
-function Card() {
+import { motion } from "framer-motion"
+function Card({data,reference}) {
   return (
-     <div className='relative w-40 h-52 px-3 py-5  rounded-[20px] text-white bg-zinc-900/90 overflow-hidden'>
+     <motion.div drag dragConstraints={reference} whileDrag={{scale:1.2}} dragElastic={0.2}  className='flex-shrink-0 relative w-40 h-52 px-3 py-5  rounded-[20px] text-white bg-zinc-900/90 overflow-hidden'>
          <FaFileAlt />
-        <p className='text-sm leading-tight mt-5 font-semibold text-zinc-200'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        <p className='text-sm leading-tight mt-5 font-semibold text-zinc-200'>{data.desc}</p>
       <div className='footer absolute bottom-0  w-full h-10  left-0 '>
         <div className=' py-2 px-5  flex items-center justify-between '>
-          <h5>.4mb</h5>
+          <h5>{data.filesize}</h5>
            <span className='w-5 h-5 bg-sky-200 rounded-full flex items-center justify-center text-zinc-900 hover:bg-red-400' >
             <MdDelete size={30} />
           </span>
           </div>
       </div>
-     </div>
+     </motion.div>
     
   )
 }
--- src/Compontents/Foreground.jsx
+++ src/Compontents/Foreground.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import {useRef, useState } from 'react'
 import Card from './Card'
+
 function Foreground() {
+   const ref = useRef(null);
+    
+  const data = [
+       {desc: "Hi , Tuhin , Stay foucused on your goal. and remember why you have started",
+        filesize: "6.9mb",
+        close: true,
+       },
+       {desc: "keep going",
+        filesize: "6.9mb",
+        close: true,
+       },
+       {desc: "Great you are consistent",
+        filesize: "6.9mb",
+        close: true,
+       }
+  ];
+  
   return (
     <>
-    <div className='fixed top-0 left-0 z-[3] w-full h-full'>
-        <Card />
-        <Card />
+    <div ref={ref} className='fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5'>
+        {data.map((item,index)=>(
+          <Card data={item} refernce={ref}/>
+        ))}
         </div>
     </>
   )
